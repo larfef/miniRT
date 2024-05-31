bf1c5c914273aaf948a9dab0474dea9df205d087
Reda Kerstens <rkersten@fu-r8-p8.s19.be>
2024-05-31 08:26:37 +0200

    fix minimal scene check

--- src/file_parsing/is_minimal_scene_valid.c
+++ src/file_parsing/is_minimal_scene_valid.c
@@ -6,7 +6,7 @@
 /*   By: rkersten <rkersten@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 12:19:09 by rkersten          #+#    #+#             */
-/*   Updated: 2024/05/29 14:27:06 by rkersten         ###   ########.fr       */
+/*   Updated: 2024/05/31 08:25:42 by rkersten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,13 @@
 
 bool	is_minimal_scene_valid(t_file *file)
 {
-	if (!file->minimal_scene[file->current_line])
-		__exit(file, E_ELEM);
+	int	i;
+
+	i = -1;
+	while (++i != 3)
+	{
+		if (!file->minimal_scene[i])
+			__exit(file, E_ELEM);
+	}
 	return (true);
 }
