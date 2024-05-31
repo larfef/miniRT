92205c83acde2f9a7176249572baea246364a157
Reda Kerstens <rkersten@mi-r8-p2.s19.be>
2024-05-31 17:29:01 +0200

    Update src/file_parsing/open_file.c

--- src/file_parsing/open_file.c
+++ src/file_parsing/open_file.c
@@ -6,7 +6,7 @@
 /*   By: rkersten <rkersten@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 13:43:43 by rkersten          #+#    #+#             */
-/*   Updated: 2024/05/31 17:22:11 by rkersten         ###   ########.fr       */
+/*   Updated: 2024/05/31 17:24:54 by rkersten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,4 @@ void	open_file(t_file *file)
 		printf("miniRT: %s\n", strerror(errno));
 		exit(1);
 	}
-	printf("%d\n", file->fd);
 }
