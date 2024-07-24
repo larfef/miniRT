9a8b066519298b8808f60bccf16eee62cc34f8b7
Reda Kerstens <rkersten@mi-c2-p6.s19.be>
2024-07-24 16:04:31 +0200

    Update

--- src/rendering/cylinder_intersection.c
+++ src/rendering/cylinder_intersection.c
@@ -1,9 +1,16 @@
-#include "../../inc/rendering.h"
-#include <math.h>
-
-//int	cylinder_intersection(t_shapes *shape, t_vector *point)
-//{
-//	t_ray	center_to_point;
+//#include "../../inc/types.h"
+//#include "../../inc/operation.h"
+//#include <math.h>
 //
-//	center_to_point = sub_vector()
+//int	cylinder_intersection(t_shapes *cylinder, t_vector *hit_point)
+//{
+//	t_vector	center = {0};
+//	t_vector	center_to_P;
+//	t_vector 	d_X_center_to_P;
+//	t_vector	P = {0};
+//
+//	center.dir = cylinder->center;
+//	P.dir = sub_point(hit_point->dir, hit_point->origin);
+//	center_to_P = sub_vector(P, center);
+//	d_X-
 //}
