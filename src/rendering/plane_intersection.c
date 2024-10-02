53be89173c6df3c797c480765e4778c1274b1919
larfef <kerstensreda@outlook.com>
2024-10-02 13:41:06 +0200

    norm

--- src/rendering/plane_intersection.c
+++ src/rendering/plane_intersection.c
@@ -1,61 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   plane_intersection.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rkersten <rkersten@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 14:32:06 by rkersten          #+#    #+#             */
+/*   Updated: 2024/10/02 13:28:20 by rkersten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft/inc/libft.h"
 #include "../../inc/operation.h"
 #include "../../inc/constant.h"
 #include <math.h>
 
-// float   plane_linear_equation(t_vector *normal, t_point *origin, t_point *direction)
-// {
-//     float   solution;
-//     float   denominator;
-//     float   numerator;
-//     float   D;
-//
-//     D = dot_product(*normal, (t_vector){{0,0,0}, *direction});
-//     numerator = (-1 * ((normal->origin.x * origin->x)
-//                 + (normal->origin.y * origin->y)
-//                 + (normal->origin.z * origin->z))
-//                 + D);
-//     denominator = ((normal->dir.x * direction->x)
-//                     + (normal->dir.y * direction->y)
-//                     + (normal->dir.z * direction->z));
-//     if (denominator == 0)
-//         return (-1.0f);
-//     solution = numerator / denominator;
-//     if (solution > 0 )
-//         return (solution);
-//     return (-1.0f);
-// }
-
-float plane_linear_equation(t_vector *normal, t_point *point_on_plane, t_point *ray_origin, t_point *ray_direction)
+float	plane_linear_equation(t_vector *normal, t_point *point_on_plane,
+	t_point *ray_origin, t_point *ray_direction)
 {
-	float numerator;
-	float denominator;
-	float t;
+	t_vector	ray_to_plane;
+	float		numerator;
+	float		denominator;
+	float		t;
 
-	// Calculate denominator: dot product of plane normal and ray direction
-	denominator = dot_product(*normal, (t_vector){{0,0,0}, *ray_direction});
-
-	// Check if ray is parallel to the plane
-	if (fabs(denominator) < EPSILON)
-		return (-1.0f);  // No intersection, ray is parallel to the plane
-
-	// Vector from ray origin to a point on the plane
-	t_vector ray_to_plane;
+	ft_memset(&ray_to_plane, 0, sizeof(ray_to_plane));
+	denominator = dot_product(*normal, (t_vector){{0, 0, 0},
+			*ray_direction});
+	if (fabs((double)denominator) < EPSILON)
+		return (-1.0f);
 	ray_to_plane.dir = sub_point(*point_on_plane, *ray_origin);
-
-	// Calculate numerator: dot product of normal and ray_to_plane vector
 	numerator = dot_product(*normal, ray_to_plane);
-
-	// Calculate intersection point `t`
 	t = numerator / denominator;
-
-	// Ensure the intersection is in front of the ray origin
 	if (t > 0)
-		return t;
-
-	return (-1.0f);  // No valid intersection, behind ray origin
+		return (t);
+	return (-1.0f);
 }
 
-float plane_intersection(t_shapes *plane, t_vector *ray)
+float	plane_intersection(t_shapes *plane, t_vector *ray)
 {
-	return plane_linear_equation(&plane->orientation, &plane->center, &ray->origin, &ray->dir);
+	return (plane_linear_equation(&plane->orientation,
+			&plane->center, &ray->origin, &ray->dir));
 }
--- src/rendering/set_color.c
+++ src/rendering/set_color.c
@@ -15,8 +15,24 @@
 #include "../../inc/types.h"
 
 /*
- * @dev: utility function to set a gradient background color
- */
+	@dev: Creates a color structure using RGB float values.
+		  Converts the floating-point values to 8-bit
+		  unsigned integers for red, green, and blue.
+
+	@params: r: Red channel as a float value [0, 1].
+			 g: Green channel as a float value [0, 1].
+			 b: Blue channel as a float value [0, 1].
+
+	@return: A color struct containing the specified
+			 red, green, and blue channels.
+
+	@behavior:
+		1. Each float color component is multiplied by 255.999
+		   to fit into an 8-bit unsigned integer.
+		2. The red, green, and blue values are set in the
+		   color struct.
+		3. Alpha is set to 0.
+*/
 
 color	set_color(float r, float g, float b)
 {
@@ -29,15 +45,34 @@ color	set_color(float r, float g, float b)
 	return (color);
 }
 
+/*
+	@dev: Generates a sky-like color based on the direction
+		  of a ray, creating a gradient effect.
+
+	@params: direction: Pointer to a vector representing
+						the ray direction.
+
+	@return: A color struct representing the RGB color for
+			 the given ray direction.
+
+	@behavior:
+		1. Normalizes the direction vector.
+		2. Calculates an interpolation value `a` using the
+		   ray’s y-component.
+		3. Interpolates between two colors (white and a sky
+		   blue gradient) using `a`.
+		4. Returns the resulting color.
+*/
+
 color ray_color(t_vector *direction)
 {
 	float		a;
 	float		rgb[3];
 
 	normalize(direction);
-	a = 0.5 * (direction->dir.y + 1.0);
-	rgb[0] = ((1.0 - a) * 1.0) + (a * 0.5);
-	rgb[1] = ((1.0 - a) * 1.0) + (a * 0.7);
-	rgb[2] = ((1.0 - a) * 1.0) + (a * 1.0);
+	a = 0.5f * (direction->dir.y + 1.0f);
+	rgb[0] = ((1.0f - a) * 1.0f) + (a * 0.5f);
+	rgb[1] = ((1.0f - a) * 1.0f) + (a * 0.7f);
+	rgb[2] = ((1.0f - a) * 1.0f) + (a * 1.0f);
 	return (set_color(rgb[0], rgb[1], rgb[2]));
 }
